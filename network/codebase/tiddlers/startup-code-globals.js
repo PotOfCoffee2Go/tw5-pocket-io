a5869d94f1b1f19e3bb15a545fc5ecd2a583caba
PotOfCoffee2Go <PotOfCoffee2Go@users.noreply.github.com>
2025-03-30 09:45:31 -0400

    Add date to timestamps

--- network/codebase/tiddlers/startup-code-globals.js
+++ network/codebase/tiddlers/startup-code-globals.js
@@ -14,15 +14,15 @@ $rt.context.uid = () => crypto.randomUUID().split('-').pop();
 
 // Hack of Node-Red utilLog()
 $rt.context.tStamp = function (msg) {
-	// const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 	const d = new Date();
 	const time = [
 		d.getHours().toString().padStart(2, '0'),
 		d.getMinutes().toString().padStart(2, '0'),
 		d.getSeconds().toString().padStart(2, '0')
 	].join(':');
-//	return `${d.getDate()} ${months[d.getMonth()]} ${time} `;
-	return `${time} `;
+	return `${d.getDate()} ${months[d.getMonth()]} ${time} `;
+//	return `${time} `;
 }
 
 $rt.context.ttStamp = () => {
